eb225964ac8d562e7ddbea5af24370e00175ee6a
AbdulWaheed MahmoodAhmad <abdulwaheedmahmoodahmad6@gmail.com>
2024-11-27 10:01:13 +0100

    Setting up Project

---
+++ AnurStore.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnurStore.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
